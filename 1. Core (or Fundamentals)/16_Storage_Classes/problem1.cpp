da0eed7edc7483f6ea910f563f1980cb82925634
InnovatorMind <tarnado445@gmail.com>
2025-08-31 00:37:36 +0530

    Storage Classes

---
+++ 1. Core (or Fundamentals)/16_Storage_Classes/problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    f1();
+    cout << "\n";
+    return 0;
+}
+
+void f1()
+{
+    auto x = 5;
+    cout << ("%d\n", x);
+    {
+        int y = 4;
+        cout << ("%d\n", x);
+    }
+}
